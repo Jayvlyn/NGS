9c25fc8b1990533e1a3b884ae022999c0fa35e05
MikePerez2022 <123500465+MikePerez2022@users.noreply.github.com>
2025-04-03 15:36:37 -0600

    Start on settings logic & added test assets

---
+++ Assets/Mike/Scripts/DataPersistence/Data/GameData.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.IO;
+
+[System.Serializable]
+public class GameData
+{
+    public string Name;
+
+    public GameData()
+    {
+        this.Name = "";
+    }
+}
--- Assets/Mike/Scripts/gameSettings.cs
+++ Assets/Mike/Scripts/gameSettings.cs
@@ -1,7 +1,23 @@
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "gameSettings", menuName = "Scriptable Objects/gameSettings")]
+[CreateAssetMenu(fileName = "GameSettings", menuName = "Scriptable Objects/GameSettings")]
 public class GameSettings : ScriptableObject
 {
-    
+    public float masterVolume;
+    public float musicVolume;
+    public float sfxVolume;
+
+    public int screenResolution;
+
+    public bool hasPostProcessing;
+    public bool isFullScreen;
+
+    public char jumpKey;
+    public char castKey;
+    public char reelKey;
+    public char pauseKey;
+    public char releaseKey;
+    public char walkLeftKey;
+    public char interactKey;
+    public char walkRightKey;
 }
