00c86b474fea599c7375c7c0a58af7227ddb88a4
Jacob Leonardo <leonardojacob257@gmail.com>
2025-04-04 16:42:16 -0600

    Jump with buffer and coyote time

--- Assets/Jacob/Scripts/PlatformingPlayerController.cs
+++ Assets/Jacob/Scripts/PlatformingPlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -6,7 +7,6 @@ public class PlatformingPlayerController : MonoBehaviour
     [Header("References")]
     [SerializeField] private Rigidbody2D rb;
 
-
     [Header("Stats")]
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float moveVelocityLimit = 10f;
@@ -14,8 +14,15 @@ public class PlatformingPlayerController : MonoBehaviour
     [SerializeField] private float maxLineLength = 10f;
     [SerializeField] private float castSpeed = 10f;
     [SerializeField] private float reelSpeed = 10f;
-    [SerializeField] private float changeXDirSharpness = 10;
-    [SerializeField] private float groundFriction = 0.98f;
+    [SerializeField,Tooltip("When changing direction, the players curent speed is used for counteraction, this variable multiplies against the current player speed")] 
+    private float changeDirSpeedMult = 0.5f;
+    [SerializeField,Tooltip("Linear X Velocity of player will get multiplied by this value while on ground (1 = no friction)"),Range(0.8f, 1)] 
+    private float groundFriction = 0.98f;
+    [SerializeField,Tooltip("Time jump input will be stored so the player will jump again once then hit the ground")] 
+    private float jumpBufferTime = 0.1f;
+    [SerializeField] private float coyoteTime = 0.1f;
+    [SerializeField] private int totalJumps = 1;
+    private int currentJumps;
 
     [Header("Ground Check")]
     [SerializeField] private Transform groundCheckPos;
@@ -30,11 +37,14 @@ public class PlatformingPlayerController : MonoBehaviour
     private bool jumpHeld = false;
     private bool reelHeld = false;
 
-    private void Update()
+	private void Start()
+	{
+        currentJumps = totalJumps;
+	}
+
+	private void Update()
     {
         onGround = isGrounded();
-
-        Debug.Log(onGround);
 
         if(onGround)
         {
@@ -48,7 +58,7 @@ public class PlatformingPlayerController : MonoBehaviour
             
             if(onGround && ((rb.linearVelocityX > 0 && moveInput < 0) || (rb.linearVelocityX < 0 && moveInput > 0)))
             { // changing dir on ground
-                speed *= changeXDirSharpness;
+                speed *= changeDirSpeedMult;
             }
 
             // move when not moving max speed
@@ -75,13 +85,76 @@ public class PlatformingPlayerController : MonoBehaviour
         }
     }
 
+    public void OnJump(InputValue value)
+    {
+        if(currentJumps > 0 || isCoyoteTimerActive())
+        { // Do jump
+            DoJump();
+        }
+        else
+        {
+            if (jumpBuffer != null) StopCoroutine(jumpBuffer);
+            jumpBuffer = StartCoroutine(JumpBuffer(jumpBufferTime));
+        }
+    }
+
+    public void DoJump()
+    {
+        currentJumps--;
+        if (rb.linearVelocityY < 0) rb.linearVelocityY = 0;
+        rb.AddForce(Vector2.up * jumpStrength, ForceMode2D.Impulse);
+    }
+
+    public Coroutine jumpBuffer;
+    public IEnumerator JumpBuffer(float bufferTime)
+    {
+        yield return new WaitForSeconds(bufferTime);
+        jumpBuffer = null;
+    }
+
+    public bool isJumpBufferActive()
+    {
+        return jumpBuffer != null;
+    }
+
+
+    public Coroutine coyoteTimer;
+    public IEnumerator CoyoteTimer(float coyoteTime)
+    {
+        yield return new WaitForSeconds(coyoteTime);
+        coyoteTimer = null;
+    }
+
+    public bool isCoyoteTimerActive()
+    {
+        return coyoteTimer != null;
+    }
+
+
     private bool isGrounded()
     {
         if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
         {
+            if (!onGround) OnLand(); // first frame returning true, so just landed
             return true;
         }
+        // not on currently ground:
+        if(onGround) // was grounded last frame
+        {
+            if(currentJumps == totalJumps) // left ground without jumping off ground
+            {
+                currentJumps--;
+                if (coyoteTimer != null) StopCoroutine(coyoteTimer);
+                coyoteTimer = StartCoroutine(CoyoteTimer(coyoteTime));
+            }
+        }
         return false;
+    }
+    
+    private void OnLand()
+    {
+        if (isJumpBufferActive()) DoJump();
+        currentJumps = totalJumps;
     }
 
     private void OnEnable()
