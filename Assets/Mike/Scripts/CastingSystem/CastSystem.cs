23a633aed9aed15be48e297859c6d31becb7be05
MikePerez2022 <123500465+MikePerez2022@users.noreply.github.com>
2025-04-09 14:55:31 -0600

    finished cast system

--- Assets/Mike/Scripts/CastingSystem/CastSystem.cs
+++ Assets/Mike/Scripts/CastingSystem/CastSystem.cs
@@ -1,36 +1,79 @@
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 
 public class CastSystem : MonoBehaviour
 {
-    [SerializeField] GameObject CastScreen;
+    [Header("Pop-up & cast bar")]
+    [SerializeField] GameObject castScreen;
     [SerializeField] Scrollbar castBar;
 
-    private float increment = 0.01f;
+    [Header("Cast quality outcome events")]
+    [SerializeField] UnityEvent bestOutcome;
+    [SerializeField] UnityEvent normalOutcome;
+    [SerializeField] UnityEvent worstOutcome;
+
+    private float increment = 0;
+    private float increase = 0;
+    private float speed = 0;
 
     void Start()
     {
-        
+        increment = 0.01f;
+        increase = 0;
+        speed = 50;
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            CastScreen.SetActive(false);
-        }
-        if (Input.anyKeyDown)
-        {
-            print("You got a " + castBar.value.ToString());
-        }
+        if (Input.GetKeyDown(KeyCode.Escape)) ResetCast();
+        else if (Input.anyKeyDown) CheckCast(castBar.value);
+
+        SpeedVariance(castBar.value);
+
+        Rebound(castBar.value);
+
+        castBar.value += increment * (Time.deltaTime * (speed + increase));
     }
 
-    private void FixedUpdate()
+    private void CheckCast(float value)
     {
-        if (castBar.value >= 1) increment = -0.01f;
-        else if (castBar.value <= 0) increment = 0.01f;
+        ResetCast();
 
-        castBar.value += increment;
+        if (value >= 0.48f && value <= 0.52f) bestOutcome.Invoke();
+        else if (value >= 0.31f && value <= 0.69f) normalOutcome.Invoke();
+        else worstOutcome.Invoke();
     }
+
+    private void Rebound(float value)
+    {
+        if (value >= 1)
+        {
+            increment = -0.01f;
+            increase += (increase != 150) ? 5 : 0;
+        }
+        else if (value <= 0)
+        {
+            increment = 0.01f;
+            increase += (increase != 150) ? 5 : 0;
+        }
+    }
+
+    private void SpeedVariance(float value)
+    {
+        if (value >= 0.25f && value <= 0.75f) speed = 100;
+        else speed = 50;
+    }
+
+    private void ResetCast()
+    {
+        increment = 0.01f;
+        increase = 0;
+        speed = 50;
+
+        castBar.value = 0;
+        castScreen.SetActive(false);
+    }
+
 }
