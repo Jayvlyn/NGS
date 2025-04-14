ca57be0ee3f63e5b2a874775631c1e558e8134e6
MikePerez2022 <123500465+MikePerez2022@users.noreply.github.com>
2025-04-14 16:20:05 -0600

    finished mini game obstacles script

--- Assets/Mike/Scripts/Minigame/MinigameObstacle.cs
+++ Assets/Mike/Scripts/Minigame/MinigameObstacle.cs
@@ -1,3 +1,5 @@
+using GameEvents;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,8 +8,9 @@ public class MinigameObstacle : MonoBehaviour
     [Header("Obstacle references")]
     [SerializeField] float speed;
     [SerializeField] Sprite image;
+    [SerializeField] GameObject imageObject;
     [SerializeField] GameObject fishMinigame;
-    private Vector2 direction;
+    private float currentSpeed = 0.0f;
 
     [Header("Minigame effects")]
     [SerializeField] float speedPenalty;
@@ -16,18 +19,78 @@ public class MinigameObstacle : MonoBehaviour
 
     void Start()
     {
-        this.GetComponent<Image>().sprite = image;
+        imageObject.GetComponent<Image>().sprite = image;
+        currentSpeed = speed;
+        transform.Rotate(0f, 0f, 20f);
     }
 
 
     void Update()
     {
-        float randomRotation = Random.Range(25f, 45f);
-        gameObject.transform.Rotate(0f, 0f, 15 * Time.deltaTime);
-        gameObject.transform.Translate(0, 50 * Time.deltaTime, 0);
-        //this.transform.forward.Normalize() += speed;
-
+        MoveObstacle();
     }
+
+    #region SwimLogic
+
+    void MoveObstacle()
+    {
+        // Checks
+
+        // Keep the fish within bounds of the minigame
+        if ((transform.localPosition.x >= 500 || transform.localPosition.x <= -500))
+        {
+            BounceOff();
+            return;
+        }
+
+        if ((transform.localPosition.y >= 300 || transform.localPosition.y <= -450))
+        {
+            BounceOff(true);
+            return;
+        }
+
+        imageObject.transform.Rotate(0f,0f, 45f * Time.deltaTime);
+
+        Vector2 direction = new Vector2(transform.right.x, transform.right.y);
+
+        direction.Normalize();
+        direction.x *= currentSpeed * Time.deltaTime;
+        direction.y *= currentSpeed * Time.deltaTime;
+
+        Vector2 newPosition = new Vector2(transform.localPosition.x + direction.x, transform.localPosition.y + direction.y);
+
+        transform.localPosition = newPosition;
+    }
+
+    void BounceOff(bool top = false)
+    {
+        Vector3 newAngle = transform.localEulerAngles;
+        newAngle.z *= -1;
+        transform.localEulerAngles = newAngle;
+
+        // Ensure fish goes back into bounds
+        if (top)
+        {
+            Vector2 tempPos = transform.localPosition;
+            if (tempPos.y > 0) tempPos.y = 299;
+            else tempPos.y = -449;
+            transform.localPosition = tempPos;
+        }
+        else
+        {
+            Vector2 tempPos = transform.localPosition;
+            if (tempPos.x > 0) tempPos.x = 499;
+            else tempPos.x = -499;
+            transform.localPosition = tempPos;
+            
+            // Switch Direction
+            currentSpeed *= -1;
+        }
+
+        Debug.Log("Bounce off bounds");
+    }
+
+    #endregion
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
