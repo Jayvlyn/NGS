82a9cd4fb9a55497972ac73231e7820075ad9e1f
Jacob Leonardo <leonardojacob257@gmail.com>
2025-04-14 13:20:49 -0600

    Input System for boss minigame

--- Assets/Scripts/Bossfight/BossfightPlayerController.cs
+++ Assets/Scripts/Bossfight/BossfightPlayerController.cs
@@ -1,18 +1,12 @@
 using System.Collections.Generic;
 using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.InputSystem;
 [RequireComponent (typeof(Rigidbody2D))]
 [RequireComponent(typeof(DistanceJoint2D))]
 
 public class BossfightPlayerController : MonoBehaviour
 {
-    //Input will be changed to event system after basic functionality is confirmed
-    [SerializeField] private KeyCode upKey = KeyCode.W;
-    [SerializeField] private KeyCode downKey = KeyCode.S;
-    [SerializeField] private KeyCode leftKey = KeyCode.A;
-    [SerializeField] private KeyCode rightKey = KeyCode.D;
-    [SerializeField] private KeyCode reelKey = KeyCode.E;
-    [SerializeField] private KeyCode slackKey = KeyCode.Q;
     [SerializeField] private float movementSpeed = 5.0f;
     [SerializeField] private float reelSpeed = 1.0f;
     [SerializeField] private BossFishController boss;
@@ -32,45 +26,94 @@ public class BossfightPlayerController : MonoBehaviour
     {
         float currentDistance = Vector3.Distance(boss.transform.position, transform.position);
         Vector3 movement = new(0, 0);
-        if (Input.GetKey(rightKey))
+        if (moveInput != Vector2.zero)
         {
-            movement.x++;
-        }
-        if (Input.GetKey(leftKey))
-        {
-            movement.x--;
-        }
-        if (Input.GetKey(upKey))
-        {
-            movement.y++;
-        }
-        if(Input.GetKey(downKey))
-        {
-            movement.y--;
-        }
-        if(movement.magnitude > 0)
-        {
-            movement = movementSpeed * Time.deltaTime * movement.normalized;
+            movement = movementSpeed * Time.deltaTime * moveInput.normalized;
             body.MovePosition(transform.position + movement);
         }
-        if(Input.GetKey(reelKey))
+        if (holdingReel)
         {
             joint.distance -= reelSpeed * Time.deltaTime * Mathf.Pow(joint.distance * 0.2f, 2);
         }
-        if(Input.GetKey(slackKey) || activeBlockers.Count > 0)
+        if (holdingSlack || activeBlockers.Count > 0)
         {
-            joint.enabled = false;
             joint.distance = currentDistance;
-        }
-        if(Input.GetKeyUp(slackKey))
-        {
-            joint.enabled = true;
         }
         body.linearVelocity = Vector3.zero;
         body.angularVelocity = 0;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    #region INPUT
+
+    private Vector2 moveInput;
+	public void OnMove(InputValue value)
+	{
+        moveInput = value.Get<Vector2>();
+	}
+
+    private bool holdingReel;
+    public void OnReel(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            holdingReel = true;
+        }
+        else
+        {
+            holdingReel = false;
+        }
+    }
+
+    private bool holdingSlack;
+    public void OnSlack(InputValue value)
+    {
+		if (value.isPressed)
+		{
+            holdingSlack = true;
+			joint.enabled = false;
+		}
+		else
+		{
+            holdingReel = false;
+			joint.enabled = true;
+		}
+	}
+
+	#endregion
+
+	#region INPUT HOLDING HANDLING
+
+	private void OnEnable()
+	{
+		Application.focusChanged += OnFocusChanged;
+	}
+
+	private void OnDisable()
+	{
+		Application.focusChanged -= OnFocusChanged;
+	}
+
+	private void OnFocusChanged(bool hasFocus)
+	{
+		if (hasFocus)
+		{
+		}
+		else
+		{ // game lost focus aka player tabbed out
+			UnholdAllInputs(); // player is no longer holding input if tabbed out
+		}
+	}
+
+	private void UnholdAllInputs()
+	{
+        moveInput = Vector2.zero;
+        holdingReel = false;
+        holdingSlack = false;
+	}
+
+	#endregion
+
+	private void OnCollisionEnter2D(Collision2D collision)
     {
         Vector3 relPos = joint.connectedBody.transform.position - transform.position;
         float angle = Vector3.Angle(collision.GetContact(0).normal, relPos);
