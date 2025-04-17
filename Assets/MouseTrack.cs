286f3dfbad396d629bd662ddad95d33a45920d58
Jacob Leonardo <leonardojacob257@gmail.com>
2025-04-17 13:38:30 -0600

    Refined Camera

--- Assets/MouseTrack.cs
+++ Assets/MouseTrack.cs
@@ -5,7 +5,6 @@ public class MouseTrack : MonoBehaviour
 	private void Update()
 	{
 		Vector3 mousePosition = Input.mousePosition;
-
 		mousePosition.x = Mathf.Clamp(mousePosition.x, 0, Screen.width);
 		mousePosition.y = Mathf.Clamp(mousePosition.y, 0, Screen.height);
 
