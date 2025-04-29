eb892f66438afb0cfd511f64a88e9c9379fb9057
Zeadrasil <thewitepheonix@gmail.com>
2025-04-28 19:49:17 -0600

    Quest Data Struct

---
+++ Assets/Scripts/Structs/QuestData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+[System.Serializable]
+public struct QuestData
+{
+    public string FishName;
+    public string QuestDescription;
+    public float MinLength;
+    public float Reward;
+}
