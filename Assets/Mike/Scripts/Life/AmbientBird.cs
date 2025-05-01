d7e8a21ddef14c81093dfc2bc9a3b3913b39241c
Jacob Leonardo <leonardojacob257@gmail.com>
2025-05-01 15:36:27 -0600

    Working bird

--- Assets/Mike/Scripts/Life/AmbientBird.cs
+++ Assets/Mike/Scripts/Life/AmbientBird.cs
@@ -2,13 +2,63 @@ using UnityEngine;
 
 public class AmbientBird : MonoBehaviour
 {
-	public Animator animator;
+	[Header("References")]
+	[SerializeField] private Animator animator;
+	[Header("Tweaking")]
+	[SerializeField] private float minFlySpeed = 2;
+	[SerializeField] private float maxFlySpeed = 4;
+	[SerializeField] private float eatTimeLowerLimit = 2;
+	[SerializeField] private float eatTimeUpperLimit = 10;
+	private float flySpeed;
+	private float eatTimer;
+	private bool flying;
+	private Transform playerT;
+	private Vector2 target;
+
+	private void Start()
+	{
+		StartEatTimer();
+		flySpeed = Random.Range(minFlySpeed, maxFlySpeed);
+		FlipX(Mathf.Sign(Random.Range(-1f, 1f)));
+	}
+
+	private void Update()
+	{
+		if (eatTimer > 0)
+		{
+			eatTimer -= Time.deltaTime;
+			if (eatTimer < 0)
+			{
+				animator.SetTrigger("ToEatingBird");
+				StartEatTimer();
+			}
+		}
+		if(flying)
+		{
+			transform.position = Vector3.MoveTowards(transform.position, target, flySpeed * Time.deltaTime);
+			if (Vector2.Distance(transform.position, playerT.position) > 30)
+			{
+				Destroy(gameObject);
+			}
+		}
+	}
+
+	private void StartEatTimer()
+	{
+		eatTimer = Random.Range(eatTimeLowerLimit, eatTimeUpperLimit);
+	}
+
+	private void FlipX(float dir)
+	{
+		transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x) * dir, transform.localScale.y);
+	}
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if(collision.gameObject.CompareTag("Player"))
 		{
 			FlyOff(Mathf.Sign(transform.position.x - collision.transform.position.x));
+			playerT = collision.transform;
 		}
 	}
 
@@ -18,8 +68,9 @@ public class AmbientBird : MonoBehaviour
 	/// <param name="dir">-1 = left, 1 = right</param>
 	private void FlyOff(float dir)
 	{
+		flying = true;
 		animator.SetTrigger("ToFlyingBird");
-		transform.localScale = new Vector2(Mathf.Abs(transform.localScale.x) * dir, transform.localScale.y);
-		//Vector3.MoveTowards(transform.position, transform.right )
+		FlipX(dir);
+		target = new Vector2((transform.position.x + 100) * Mathf.Sign(transform.localScale.x), transform.position.y + Random.Range(20, 100));
 	}
 }
