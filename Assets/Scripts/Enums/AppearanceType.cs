3c00d8e88b2b736b57027996fbe1ac8a196d6fd5
Zeadrasil <thewitepheonix@gmail.com>
2025-05-07 13:17:36 -0600

    Add In Classes

---
+++ Assets/Scripts/Enums/AppearanceType.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public enum AppearanceType
+{
+    Teleport,
+    FadeIn,
+    ZoomIn,
+    FromBottom,
+    FromTop,
+    FromLeft,
+    FromRight
+}
---
+++ Assets/Scripts/Structs/PopupAppearanceData.cs
@@ -0,0 +1,8 @@
+using System;
+using UnityEngine;
+[Serializable]
+public struct PopupAppearanceData
+{
+    public AppearanceType AppearanceType;
+    public int time;
+}
