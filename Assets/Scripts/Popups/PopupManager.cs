01e15b6e02351537095ee463ac5a4c9bc67783aa
Jacob Leonardo <leonardojacob257@gmail.com>
2025-05-08 16:13:07 -0600

    Fix

--- Assets/Scripts/Popups/PopupManager.cs
+++ Assets/Scripts/Popups/PopupManager.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PopupManager : Singleton<PopupManager>
 {
