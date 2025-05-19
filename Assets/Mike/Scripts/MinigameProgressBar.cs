b49c9ccdd8d2d88384380ff5101954b86f140bef
MikePerez2022 <123500465+MikePerez2022@users.noreply.github.com>
2025-05-19 14:49:09 -0600

    Updated UI prefabs & create progress bar effects

---
+++ Assets/Mike/Scripts/MinigameProgressBar.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinigameProgressBar : MonoBehaviour
+{
+    [SerializeField] Slider progressBar;
+    [SerializeField] Image progressBarFill;
+
+    [SerializeField] float upperThreshold = 0.75f;
+    [SerializeField] float lowerThreshold = 0.1f;
+
+    private float flashTime = 0.2f;
+    private bool isFlashing = false;
+
+    private Color highColor = Color.green;
+    private Color midColor = Color.yellow;
+    private Color lowColor = Color.red;
+
+    private float value = 0;
+
+    void Update()
+    {
+        value = progressBar.value;
+
+        if (value > upperThreshold)
+        {
+            StopFlashing();
+            float t = Mathf.InverseLerp(1f, upperThreshold, value);
+            progressBarFill.color = Color.Lerp(highColor, midColor, t);
+        }
+        else if (value > lowerThreshold)
+        {
+            StopFlashing();
+            float t = Mathf.InverseLerp(upperThreshold, lowerThreshold, value);
+            progressBarFill.color = Color.Lerp(midColor, lowColor, t);
+        }
+        else
+        {
+            StartFlashing();
+        }
+    }
+
+    private void StartFlashing()
+    {
+        if(!isFlashing)
+        {
+            isFlashing = true;
+            StartCoroutine(Flash());
+        }
+    }
+
+    private void StopFlashing()
+    {
+        if (isFlashing)
+        {
+            StopCoroutine(Flash());
+            progressBarFill.color = lowColor;
+            isFlashing = false;
+        }
+    }
+
+    private IEnumerator Flash()
+    {
+        while (isFlashing)
+        {
+            progressBarFill.color = Color.white;
+            yield return new WaitForSeconds(flashTime);
+            progressBarFill.color = lowColor;
+            yield return new WaitForSeconds(flashTime + 0.1f);
+        }
+    }
+}
