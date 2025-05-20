5b3bf03ee7dfbb9df0e7e55304edc4f9258bdfdd
Jacob Leonardo <leonardojacob257@gmail.com>
2025-05-20 14:26:18 -0600

    Debug Scene Changer

---
+++ Assets/Scripts/Debug/DebugSceneChanger.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class DebugSceneChanger : MonoBehaviour
+{
+#if UNITY_EDITOR
+	void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Alpha1))
+		{
+			SceneLoader.LoadScene("GameScene");
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha2))
+		{
+			SceneLoader.LoadScene("Desert");
+		}
+		else if (Input.GetKeyDown(KeyCode.Alpha3))
+		{
+			SceneLoader.LoadScene("Snow");
+		}
+	}
+#endif
+}
