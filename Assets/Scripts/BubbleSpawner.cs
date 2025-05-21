db9f352dcfe03656703e9c28c570e9ce9b02b71e
Jacob Leonardo <leonardojacob257@gmail.com>
2025-05-21 13:38:20 -0600

    Fixing bubbles

--- Assets/Scripts/BubbleSpawner.cs
+++ Assets/Scripts/BubbleSpawner.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BubbleSpawner : MonoBehaviour
 {
@@ -8,12 +9,14 @@ public class BubbleSpawner : MonoBehaviour
 	[SerializeField] private float minSpawnInterval = 0.2f;
 	[SerializeField] private float maxSpawnInterval = 0.4f;
 	private float timer;
+	List<GameObject> activeBubbles = new List<GameObject>();
 
 	private void Update()
 	{
 		if(timer <= 0)
 		{
 			GameObject obj = pool.Get(transform.position, Quaternion.identity);
+			activeBubbles.Add(obj);
 			StartCoroutine(ReturnAfterDelay(obj, 5f));
 			StartCoroutine(ReturnIfPastHeight(obj, 900));
 			timer = Random.Range(minSpawnInterval, maxSpawnInterval);
@@ -24,12 +27,24 @@ public class BubbleSpawner : MonoBehaviour
 		}
 	}
 
+	private void OnDisable()
+	{
+		foreach(GameObject bubble in activeBubbles)
+		{
+			pool.Return(bubble);
+		}
+	}
+
 	private IEnumerator ReturnIfPastHeight(GameObject obj, float height)
 	{
 		while(obj != null && obj.activeSelf)
 		{
 			//Debug.Log(obj.transform.position.y);
-			if (obj.transform.position.y > height) pool.Return(obj); // higher = lower number
+			if (obj.transform.position.y > height)
+			{
+				pool.Return(obj); // higher = lower number
+				activeBubbles.Remove(obj);
+			}
 			yield return null;
 		}
 	}
@@ -40,6 +55,7 @@ public class BubbleSpawner : MonoBehaviour
 		if (obj != null && obj.activeSelf)
 		{
 			pool.Return(obj);
+			activeBubbles.Remove(obj);
 		}
 	}
 }
