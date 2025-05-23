1796498a1eb26e2cfed6b91c7111ef2a90793c18
Zeadrasil <thewitepheonix@gmail.com>
2025-05-23 15:03:25 -0600

    Update MapDisplay.cs
    Continue working on Map

--- Assets/Scripts/MapDisplay.cs
+++ Assets/Scripts/MapDisplay.cs
@@ -1,6 +1,83 @@
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MapDisplay : Singleton<MapDisplay>
 {
-    
+    [SerializeField] Tilemap[] importMaps;
+    [SerializeField] Tilemap exportMap;
+    [SerializeField] float minZoom;
+    [SerializeField] float maxZoom;
+
+    public Vector3Int WorldToCell(Vector3 world, int mapIndex = 0)
+    {
+        if(mapIndex > -1 && mapIndex < importMaps.Length && importMaps[mapIndex] != null)
+        {
+            return importMaps[mapIndex].WorldToCell(world);
+        }
+        return new Vector3Int((int)world.x, (int)world.y, (int)world.z);
+    }
+    public void Display()
+    {
+        BoundsInt bounds = importMaps[0].cellBounds;
+        foreach(Tilemap tilemap in importMaps)
+        {
+            if(tilemap != null)
+            {
+                if (tilemap.cellBounds.xMin < bounds.xMin)
+                {
+                    bounds.xMin = tilemap.cellBounds.xMin;
+                }
+                if (tilemap.cellBounds.yMin < bounds.yMin)
+                {
+                    bounds.yMin = tilemap.cellBounds.yMin;
+                }
+                if (tilemap.cellBounds.xMax > bounds.xMax)
+                {
+                    bounds.xMax = tilemap.cellBounds.xMax;
+                }
+                if (tilemap.cellBounds.yMax > bounds.yMax)
+                {
+                    bounds.yMax = tilemap.cellBounds.yMax;
+                }
+            }
+        }
+
+        for(int x = bounds.xMin; x < bounds.xMax; x++)
+        {
+            for(int y = bounds.yMin; y < bounds.yMax; y++)
+            {
+                bool notFound = true;
+                foreach(Tilemap tilemap in importMaps)
+                {
+                    if(tilemap.GetTile(new Vector3Int(x, y)) != null)
+                    {
+                        exportMap.SetTile(new Vector3Int(x, y), tilemap.GetTile(new Vector3Int(x, y)));
+                        notFound = false;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    public void ZoomIn()
+    {
+        Vector3 result = exportMap.transform.localScale;
+        result.x = Mathf.Clamp(result.x + 0.05f, minZoom, maxZoom);
+        result.y = Mathf.Clamp(result.y + 0.05f, minZoom, maxZoom);
+        exportMap.transform.localScale = result;
+    }
+
+    public void ZoomOut()
+    {
+        Vector3 result = exportMap.transform.localScale;
+        result.x = Mathf.Clamp(result.x + 0.05f, minZoom, maxZoom);
+        result.y = Mathf.Clamp(result.y + 0.05f, minZoom, maxZoom);
+        exportMap.transform.localScale = result;
+    }
+
+    public void ChangePos()
+    {
+
+    }
 }
