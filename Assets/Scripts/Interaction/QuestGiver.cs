2735903c6c9869e110d9b284787428919afcdd5e
Jacob Leonardo <leonardojacob257@gmail.com>
2025-05-23 16:24:42 -0600

    Quest Updates

--- Assets/Scripts/Interaction/QuestGiver.cs
+++ Assets/Scripts/Interaction/QuestGiver.cs
@@ -113,10 +113,6 @@ public class QuestGiver : InteractableObject
                 completedAQuest = true;
                 //TODO: Attach system to add cosmetics
             }
-            else
-            {
-                Destroy(gameObject);
-            }
         }
         if(currentPopup != null && currentPopup.TryGetComponent(out DialogueVoidPopup pop))
         {
