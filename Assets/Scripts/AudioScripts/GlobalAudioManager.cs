185450e27a7aac3354ba68f42a9ecb0be04eeb9f
Jacob Leonardo <leonardojacob257@gmail.com>
2025-05-31 20:26:32 -0600

    GlobalAudioManager GO

--- Assets/Scripts/AudioScripts/GlobalAudioManager.cs
+++ Assets/Scripts/AudioScripts/GlobalAudioManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-public class GlobalAudioManager : MonoBehaviour
+public class GlobalAudioManager : Singleton<GlobalAudioManager>
 {
     [Header("Audio Sources")]
     [SerializeField] AudioSource loopingAudioSource;
